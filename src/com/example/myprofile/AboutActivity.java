cc1733485579be9136e5d6f54e9a554bf1754bd7
AlexForce <falexson@gmail.com>
2014-04-05 00:47:46 -0400

    Remove settings from action bar

--- src/com/example/myprofile/AboutActivity.java
+++ src/com/example/myprofile/AboutActivity.java
@@ -47,7 +47,7 @@ public class AboutActivity extends ActionBarActivity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.action_about) {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
