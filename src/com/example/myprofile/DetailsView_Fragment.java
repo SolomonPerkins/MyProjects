dfbbccdc424c2d9a55830983b70463ceee561d04
AlexForce <falexson@gmail.com>
2014-04-22 08:03:02 -0400

    Include private variables which will be used to update details view

--- src/com/example/myprofile/DetailsView_Fragment.java
+++ src/com/example/myprofile/DetailsView_Fragment.java
@@ -12,11 +12,18 @@ import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.ViewFlipper;
 
 public class DetailsView_Fragment extends Fragment{
 		
 	private final GestureDetector detector = new GestureDetector(new SwipeGestureDetector());
+	
+	private String introductionMessage;
+	private String descriptionMessage;
+	private ViewFlipper slideShow;
+	private ListView features;
+	
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater,
@@ -42,4 +49,15 @@ public class DetailsView_Fragment extends Fragment{
 		
 	}
 	
+	/**
+	 * TODO: Create function which will be used to call the service. The service should return all the details relating to the product
+	 */
+	
+	/**
+	 * Calls all the other child methods to set each data
+	 * 
+	 */
+	public void setDetailsData(){
+		
+	}
 }
